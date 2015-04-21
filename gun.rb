7be955044a48dc34bad3dbc1855cc8c03f0f524e
eugene shved <snuggleshved@gmail.com>
2015-04-21 21:29:57 +0300

    Add core

--- gun.rb
+++ gun.rb
@@ -1,11 +1,12 @@
 class Gun
   def initialize
-    @lines = File.open('shoot.txt').read.split("\n")
+    @guns = File.open('shoot.txt').read.split("\n")
   end
 
   def process
     gun_writer
     add_wick
+    add_core
   end
 
   private
@@ -16,27 +17,41 @@ class Gun
 
   def gun_writer
     clean_screen
-    puts @lines
+    puts @guns
   end
 
   def wick(x, y)
-    @lines[x - 1][y]  = ' '
-    @lines[x][y]  = '@'
+    @guns[x - 1][y]  = ' '
+    @guns[x][y]  = '@'
     gun_writer
   end
 
   def add_wick
-    wait
-    wick(1, 26)
-    wait
-    wick(2, 26)
-    wait
-    wick(3, 26)
-
+    (1..3).to_a.each do |elem|
+      wait(0.5)
+      wick(elem, 26)
+    end
   end
 
-  def wait
-    sleep(0.5)
+  def wait(x)
+    sleep(x)
+  end
+
+  def core(x, y1 ,y2 , symbol)
+    @guns[x][y1..y2] = symbol*6
+    @guns[x + 1][y1 - 2..y2 + 2] = symbol*10
+    @guns[x + 2][y1 - 3..y2 + 3] = symbol*12
+    @guns[x + 3][y1 - 2..y2 + 2] = symbol*10
+    @guns[x + 4][y1..y2] = symbol*6
+    gun_writer
+  end
+
+  def add_core
+    (60..110).to_a.each do |elem|
+      core(4, elem, elem+5, 'Z')
+      wait(0.01)
+      core(4, elem, elem+5, '-')
+    end
   end
 end
 
