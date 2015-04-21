b70af00dbadae19c60edddd5817fff0ee85c64b4
eugene shved <snuggleshved@gmail.com>
2015-04-21 20:41:14 +0300

    Add wick

--- gun.rb
+++ gun.rb
@@ -4,6 +4,8 @@ class Gun
   end
 
   def process
+    gun_writer
+    add_wick
   end
 
   private
@@ -13,8 +15,30 @@ class Gun
   end
 
   def gun_writer
+    clean_screen
+    puts @lines
+  end
+
+  def wick(x, y)
+    @lines[x - 1][y]  = ' '
+    @lines[x][y]  = '@'
+    gun_writer
+  end
+
+  def add_wick
+    wait
+    wick(1, 26)
+    wait
+    wick(2, 26)
+    wait
+    wick(3, 26)
 
   end
 
+  def wait
+    sleep(0.5)
+  end
 end
 
+gun = Gun.new
+gun.process
